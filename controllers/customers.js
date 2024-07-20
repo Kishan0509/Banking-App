33968c22671e28cb5b1539c6113a7ac5175c1c4d
Kishan0509 <kishan.u0509@gmail.com>
2024-07-20 18:44:29 +0530

    Your commit message

--- controllers/customers.js
+++ controllers/customers.js
@@ -2,6 +2,5 @@ const pool=require("../utils/dbconfig");
 
 module.exports.fetch_customers=async (req,res)=>{
     const users=await pool.query("select * from user12");
-    console.log("users",users);
     return users.rows;
 }
